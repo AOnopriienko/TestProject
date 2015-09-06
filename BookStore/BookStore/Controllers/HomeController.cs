3545b6796808d97467bf445f1df3c37935d8d10a
AOnopriienko <a.onopriienko@gmail.com>
2015-09-07 02:25:20 +0300

    Add different ActionResult(Work with Controllers)

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BookStore.Models;
+using BookStore.Util;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.IO;
 
 namespace BookStore.Controllers
 {
@@ -51,6 +55,79 @@ namespace BookStore.Controllers
             int h = Int32.Parse(Request.Params["h"]);
             double s = a * h / 2;
             return "<h2>Площадь треугольника с основанием " + a + " и высотой " + h + " равна " + s + "</h2>";
+        }
+
+        public ActionResult GetHtml(int i)
+        {
+            HttpContext.Response.Cookies["ID"].Value = "Hello from MVC";
+            if (i == 1)
+            {
+                //return new HttpStatusCodeResult(404);
+                return new HttpUnauthorizedResult();
+            }
+            else if (i == 3) 
+            {
+                return new HtmlResult("<h2>" + IndexString() + "</h2>");
+            }
+            else
+            {
+                return new HtmlResult("<h2>Привет мир!</h2>");
+            }
+        }
+
+        public ActionResult GetImage()
+        {
+            string path = "../Images/visualstudio.png";
+            return new ImageResult(path);
+        }
+        public FileResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Server.MapPath("~/Images/visualstudio.png");
+            // Тип файла - content-type
+            string file_type = "application/png";
+            // Имя файла - необязательно
+            string file_name = "visualstudio.png";
+            return File(file_path, file_type, file_name);
+        }
+        // Отправка массива байтов
+        public FileResult GetBytes()
+        {
+            string path = Server.MapPath("~/Images/visualstudio.png");
+            byte[] mas = System.IO.File.ReadAllBytes(path);
+            string file_type = "application/png";
+            string file_name = "visualstudio.png";
+            return File(mas, file_type, file_name);
+        }
+        // Отправка потока
+        public FileResult GetStream()
+        {
+            string path = Server.MapPath("~/Images/visualstudio.png");
+            // Объект Stream
+            FileStream fs = new FileStream(path, FileMode.Open);
+            string file_type = "application/png";
+            string file_name = "visualstudio.png";
+            return File(fs, file_type, file_name);
+        }
+
+        public string IndexString()
+        {
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string cookiesID = HttpContext.Request.Cookies["ID"].Value;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p><p>ID from Cookies: " + cookiesID + "</p>";
+        }
+
+        // асинхронный метод
+        public async Task<ActionResult> BookList()
+        {
+            IEnumerable<Book> books = await Task.Run(() => db.Books);
+            ViewBag.Books = books;
+            return View("Index");
         }
     }
 }
